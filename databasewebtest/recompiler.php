5dda49740a6d44166c77791a0e884920caeb24e4
Aitorelc0d3 <96843952+Aitorelc0d3@users.noreply.github.com>
2024-11-27 11:48:28 +0100

    Add files via upload

---
+++ databasewebtest/recompiler.php
@@ -0,0 +1,20 @@
+<html>
+	<head>
+	<title>data</title>
+	</head>
+	
+	<body>
+	<?php 
+		  
+		  $nombre=$_POST["inputname"]; 
+		  $email=$_POST["email"]; 
+		  
+	?>
+	
+	Welcome <?php echo $email; ?><br />
+	
+	Of E-mail <?php echo $nombre; ?>
+
+	</body>
+
+</html>
